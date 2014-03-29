5a3e318a5ad3612b77fefb0f6458c220a232ad73
Trajan Przybylski <trajan.przybylski@gmail.com>
2014-03-29 22:24:04 +0000

    first commit - clean SilverStripe installation

---
+++ mysite/_config.php
@@ -0,0 +1,12 @@
+<?php
+
+global $project;
+$project = 'MGM_Project';
+
+global $database;
+$database = 'mgm';
+
+require_once('conf/ConfigureFromEnv.php');
+
+// Set the site locale
+i18n::set_locale('en_US');
